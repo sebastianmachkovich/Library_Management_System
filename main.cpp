3ff3a72990426e354f6b203a56509ae7a53e51e2
Jett-Moericke <Jettmoericke@gmail.com>
2025-05-05 09:43:49 -0700

    Update main.cpp
    Started the main

--- main.cpp
+++ main.cpp
@@ -13,5 +13,32 @@ int main()
   // 2. Admin (Librarian) Login
   // 3. User Login
   // 4. Exit
+  
+int userInput;
+  cout << "Welcome to the Library Managment System!" << endl;
+  cout << "Choose an option: " << endl;
+  cout << "1. Register New Users" << endl;
+  cout << "2. Admin (Librarian) Login" << endl;
+  cout << "3. User Login" << endl;
+  cout << "4. Exit" << endl;
+  cin >> userInput;
+  
+  if(userInput < 1 || userInput > 4){
+    cout << "Please enter a number between 1 and 4" << endl;
+    cin >> userInput;
+  }
+
+  if(userInput == 1){
+  
+  }
+  else if(userInput == 2){
+  
+  }
+  else if(userInput == 3){
+  
+  }
+  else {
+  cout << "Thank you for useing the Library Managment System, have a great day!" << endl;
+  }
   return 0;
 }
