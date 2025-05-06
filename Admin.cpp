583d5c507aafd1b7883beb3486fc7770c1e2a06e
Sebastian Machkovich <sebastian.machkovich@gmail.com>
2025-05-06 13:40:51 -0500

    Admin

--- Admin.cpp
+++ Admin.cpp
@@ -1,5 +1,5 @@
 #include "Admin.h"
-#include "Book.h"
+#include "Book.h"#include "SearchFunction.h"
 #include <fstream>
 #include <sstream>
 
@@ -66,14 +66,124 @@ bool Admin::editInventoryItem(const string &itemId, const string &field, const s
 {
   if (!isLoggedIn)
     return false;
-  // Implementation will be added later
-  return false;
+
+  ifstream inFile("books.csv");
+  ofstream outFile("books_temp.csv");
+  string line;
+  bool found = false;
+  vector<string> headers;
+  bool isHeader = true;
+
+  while (getline(inFile, line))
+  {
+    stringstream ss(line);
+    string fieldVal;
+    vector<string> fields;
+    while (getline(ss, fieldVal, ','))
+      fields.push_back(fieldVal);
+
+    if (isHeader)
+    {
+      headers = fields;
+      outFile << line << "\n";
+      isHeader = false;
+      continue;
+    }
+
+    if (fields[0] == itemId)
+    {
+      for (size_t i = 0; i < headers.size(); ++i)
+      {
+        if (headers[i] == field)
+        {
+          fields[i] = newValue;
+          found = true;
+        }
+      }
+    }
+    outFile << fields[0];
+    for (size_t i = 1; i < fields.size(); ++i)
+      outFile << "," << fields[i];
+    outFile << "\n";
+  }
+  inFile.close();
+  outFile.close();
+
+  if (found)
+  {
+    remove("books.csv");
+    rename("books_temp.csv", "books.csv");
+  }
+  else
+  {
+    remove("books_temp.csv");
+  }
+  return found;
+}
+
+bool Admin::editUserDetails(const string &libraryId, const string &field, const string &newValue)
+{
+  if (!isLoggedIn)
+    return false;
+
+  ifstream inFile("users.csv");
+  ofstream outFile("users_temp.csv");
+  string line;
+  bool found = false;
+  vector<string> headers;
+  bool isHeader = true;
+
+  while (getline(inFile, line))
+  {
+    stringstream ss(line);
+    string fieldVal;
+    vector<string> fields;
+    while (getline(ss, fieldVal, ','))
+      fields.push_back(fieldVal);
+
+    if (isHeader)
+    {
+      headers = fields;
+      outFile << line << "\n";
+      isHeader = false;
+      continue;
+    }
+
+    if (fields[0] == libraryId)
+    {
+      for (size_t i = 0; i < headers.size(); ++i)
+      {
+        if (headers[i] == field)
+        {
+          fields[i] = newValue;
+          found = true;
+        }
+      }
+    }
+    outFile << fields[0];
+    for (size_t i = 1; i < fields.size(); ++i)
+      outFile << "," << fields[i];
+    outFile << "\n";
+  }
+  inFile.close();
+  outFile.close();
+
+  if (found)
+  {
+    remove("users.csv");
+    rename("users_temp.csv", "users.csv");
+  }
+  else
+  {
+    remove("users_temp.csv");
+  }
+  return found;
 }
 
 int Admin::getActiveUserCount() const
 {
   if (!isLoggedIn)
-    return -1;
+    return -1; 
 
   ifstream file("users.csv");
   string line;
