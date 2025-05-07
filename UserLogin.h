87d92cc6672026a951cb9db689862c84783e9e3b
Jett-Moericke <Jettmoericke@gmail.com>
2025-05-07 11:32:48 -0700

    Update UserLogin.h

--- UserLogin.h
+++ UserLogin.h
@@ -7,7 +7,10 @@ class UserLogin
 {
 public:
   bool login(string &userId);
+  void userOptions(const string &userId);
   void printUserSummary(const string &userId);
+  void borrowingHistory(const string &userId);
+  Void currentlyBorrowed(const string &userId);
 };
 
 #endif
